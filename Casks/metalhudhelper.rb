e61ff59fe5605186da2095408e2c7658c3071398
David Oliver <david@tenthirty.net>
2025-05-14 16:09:18 -0700

    Update metalhudhelper.rb
    modified version to be 1.0.1

--- Casks/metalhudhelper.rb
+++ Casks/metalhudhelper.rb
@@ -1,5 +1,5 @@
 cask "metalhudhelper" do
-  version "refs/heads/main"
+  version "1.0.1"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   url "https://github.com/oliwonders/MetalHUDHelper/releases/download/v#{version}/MetalHUDHelper.app.zip",
